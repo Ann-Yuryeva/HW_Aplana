0523d42f28e5ecf01391dbd5f55c6000099271b4
Ann-Yuryeva <anutca57@mail.ru>
2018-10-19 12:52:19 +0300

    .

--- src/Basic_Operation.java
+++ src/Basic_Operation.java
@@ -5,21 +5,20 @@ public class Basic_Operation {
     public static void main(String[] args) {
 
         int count=0;
-        count++;
         double X,Y,Z;
         Scanner in = new Scanner(System.in);
-
+        count++;
         System.out.println("Введите число X = " );
         X = in.nextDouble();
-        System.out.println("Введено чисел = " + count++);
 
+        count++;
         System.out.println("Введите число Y = " );
         Y = in.nextDouble();
-        System.out.println("Введено чисел = " + count++);
 
+        count++;
         System.out.println("Введите число Z = ");
         Z = in.nextDouble();
-        System.out.println("Введено чисел = " + count++);
+
 
         double average = (X+Y+Z)/count;
         System.out.println("Среднее арифмитическое введённых чисел =" + average);
