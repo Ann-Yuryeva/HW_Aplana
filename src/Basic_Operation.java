58ee8305c257f378d41fc3b7b36439a9402c4229
Ann-Yuryeva <anutca57@mail.ru>
2018-10-19 12:33:47 +0300

    .

--- src/Basic_Operation.java
+++ src/Basic_Operation.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Basic_Operation {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
         int count=0;
         count++;
         double X,Y,Z;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Введите число X = " );
         X = in.nextDouble();
         System.out.println("Введено чисел = " + count++);
@@ -22,9 +24,10 @@ public class Basic_Operation {
         double average = (X+Y+Z)/count;
         System.out.println("Среднее арифмитическое введённых чисел =" + average);
 
-        double division = average/2%0;
-        System.out.println("Результат деления =" + division);
-        if (division>3) {
+        double division = average/2 ;
+        int ndiv = (int) division;
+        System.out.println("Результат деления =" + ndiv);
+        if (ndiv>3) {
             System.out.println("Программа выполнена корректно");
         } else {
             System.out.println("Программа выполнена НЕкорректно");
