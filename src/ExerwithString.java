063a6e39d6b1931989220f8df18b7b2c03766e9a
Ann-Yuryeva <anutca57@mail.ru>
2018-10-25 10:14:42 +0300

    Задача №10: Ввод строки с клавиатуры. Задание: Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.

--- src/ExerwithString.java
+++ src/ExerwithString.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class ExerwithString {
     public static void main(String[] args) {
-        String hi ="Hi";
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введи какой-нибудь текст: ");
+        String hi =in.nextLine();
         System.out.print("GM!" + hi + "!");
 
     }
