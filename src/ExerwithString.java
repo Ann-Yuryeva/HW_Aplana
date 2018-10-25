e44098657b58827905ecb6b7bc7027429767f4a0
Ann-Yuryeva <anutca57@mail.ru>
2018-10-25 11:26:57 +0300

    Задача №10: Ввод строки с клавиатуры  Задание Необходимо произвести ввод строки с клавиатуры и записать в строковую переменную. Результат вывести на экран.
     Задача №11: Удаление всех пробелов в заданном тексте
     Задание
    Для введенной с клавиатуры строки необходимо провести отсев пробелов. Результат в виде строки без пробелов вывести на экран.
    
    Задача №12: Сравнение двух строк Задание
    Необходимо провести сравнение длинны двух строк, которые были введены с клавиатуры и записаны в соответствующие переменные. Вывести на экран строку с наибольшей длинной.

--- src/ExerwithString.java
+++ src/ExerwithString.java
@@ -5,7 +5,31 @@ public class ExerwithString {
         Scanner in= new Scanner(System.in);
         System.out.println("Введи какой-нибудь текст: ");
         String hi =in.nextLine();
-        System.out.print("GM!" + hi + "!");
 
-    }
-}
+        /* Начинаем выполнение задания №1*/
+        System.out.println("Выполнение 1-ого задания: " );
+        System.out.println( "GM!" + hi + "!");
+
+        /* Начинаем выполнение задания №2*/
+        String bye = hi.replaceAll(" ", "");
+        System.out.println("Выполнение задания №2: " + bye);
+
+        /* Начинаем выполнение задания №3.
+        Необходимо провести сравнение длинны двух строк, которые были введены с клавиатуры и записаны в соответствующие переменные.
+         Вывести на экран строку с наибольшей длинной.
+         */
+        System.out.println("Ввести две строки для выполнения 3 задания: ");
+        String a = in.nextLine();
+        String b = in.nextLine();
+        int a_len =a.length();
+        int b_len =b.length();
+
+        if(a_len>b_len){
+            System.out.println("первая строка длиннее чем вторая");
+            System.out.println("первая строка : " + a);
+        }else if (a_len>b_len){
+            System.out.println("вторая строка длиннее чем первая");
+            System.out.println("вторая строка : " + b);
+        }
+            }
+        }
